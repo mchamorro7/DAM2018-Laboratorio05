667c12cd69d2fb41f20b61f0a282211025c15fa8
Mateo Chamorro <mateochamorro@hotmail.com>
2018-12-10 01:03:02 -0300

    REQ 01 - TERMINADO
    Terminado

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
@@ -1,12 +1,17 @@
 package ar.edu.utn.frsf.isi.dam.laboratorio05;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -35,7 +40,33 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap map) {
         miMapa = map;
+        //Solicitamos el permiso de ubicación ni bien entramos a la sección "Ver en mapa".
+       updateMap();
     }
+
+    private void updateMap(){
+        if (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //Si no se tiene el permiso garantizado se solicita el mismo.
+            ActivityCompat.requestPermissions((MainActivity)listener,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    123);
+            return;
+        }
+        //Habilitamos ubicación actual.
+        miMapa.setMyLocationEnabled(true);
+    }
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        //Si se obtuvo el permiso adecuado, se permite acceder a la ubicación actual.
+        if(requestCode==123){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   updateMap();
+                } else {
+                    Toast.makeText(getContext(), "ERROR.", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
     public void setListener(OnAbrirMapaListener listener) {
         this.listener = listener;
     }
